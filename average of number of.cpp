a06b1b601633974d7cff0e9fbf17be3afd3e1dbe
MaheshworDhakal <maheshwordhakal24@gmail.com>
2024-12-20 20:36:40 +0545

    add the file

---
+++ average of number of.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	float avg;
+	printf("enter any three number ");
+	scanf("%d%d%d",&a,&b,&c);
+	avg=(a+b+c)/3;
+	printf("the avg of number is %f",avg);
+	return 0;
+}
